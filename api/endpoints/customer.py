41501ab9b72a5d7b9515b84cd662f9c310ae1a0b
Никита <nik.lis@bk.ru>
2023-04-02 21:44:26 +0300

    Добавил методы для Order

--- api/endpoints/customer.py
+++ api/endpoints/customer.py
@@ -44,5 +44,13 @@ async def update_customer_info(pk: UUID,
     return await instance.update(**info.dict(exclude={"id"},
                                              exclude_none=True,
                                              exclude_unset=True,
-                                             exclude_defaults=True
+                                             exclude_defaults=True,
+                                             exclude_relations=True
                                              ))
+
+
+@router.delete("/{id}")
+async def delete_customer(pk: UUID):
+    instance = await get_customer_by_id(pk)
+    deleted_records = await instance.delete()
+    return None
--- api/endpoints/order.py
+++ api/endpoints/order.py
@@ -3,6 +3,8 @@ from uuid import UUID, uuid4
 from fastapi.routing import APIRouter
 from models import Order
 from typing import List
+import schemas
+from .customer import get_customer_by_id
 
 router = APIRouter()
 
@@ -14,12 +16,31 @@ async def get_all_orders():
 
 
 @router.get("/{id}",
-            response_model=Order)
+            response_model=schemas.OrderBaseResponseSchema)
 async def get_order_by_id(pk: UUID):
-    return await Order.objects.get_or_none(pk)
-#
-# @router.post("/",
-#              response_model=Order)
-# async def create_order(order: Order):
-#
-#TODO дописать методы вызова запросов!
+    return await Order.filter(id=pk)
+
+
+@router.post("/",
+             response_model=Order)
+async def create_order(order: schemas.OrderCreateRequestSchema, customer_id: UUID):
+    customer = await get_customer_by_id(customer_id)
+    return await Order.objects.create(customer=customer, **order.dict())
+
+
+@router.patch("/{id}",
+              response_model=Order)
+async def update_order_info(pk: UUID,
+                            info: Order) -> Order:
+    instance = await get_order_by_id(pk)
+    return await instance.update(**info.dict(exclude={"id"},
+                                             exclude_unset=True,
+                                             exclude_none=True,
+                                             exclude_defaults=True))
+
+
+@router.delete("/{id}")
+async def delete_order(pk: UUID):
+    instance = await get_order_by_id(pk)
+    return await instance.delete()
+# TODO дописать методы вызова запросов!
