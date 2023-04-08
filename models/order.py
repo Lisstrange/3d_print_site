544a7ac1a997a6b699281588c727658e6cd9d366
Никита <nik.lis@bk.ru>
2023-04-08 14:17:44 +0300

    add defaults

--- models/order.py
+++ models/order.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from enum import Enum
 import ormar
 from db import BaseMeta
@@ -27,8 +27,12 @@ class Order(ormar.Model):
                           uuid_format="string",
                           default=uuid4)
     customer: UUID = ormar.ForeignKey(Customer)
-    order_date: datetime.date = ormar.Date()
-    price: int = ormar.Integer()
-    material: MaterialTypeEnum = ormar.String(max_length=100, choices=MaterialTypeEnum, default=MaterialTypeEnum.abc)
+    order_date: datetime.date = ormar.DateTime(default=datetime.now())
+    price: int = ormar.Integer(default=0)
+    material: MaterialTypeEnum = ormar.String(max_length=100,
+                                              choices=MaterialTypeEnum,
+                                              default=MaterialTypeEnum.abc)
     archive_path: str = ormar.String(max_length=100)
-    delivery_type: DeliveryTypeEnum = ormar.String(max_length=100, choices=DeliveryTypeEnum, default=DeliveryTypeEnum.sdek)
+    delivery_type: DeliveryTypeEnum = ormar.String(max_length=100,
+                                                   choices=DeliveryTypeEnum,
+                                                   default=DeliveryTypeEnum.sdek)
